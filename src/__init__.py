b2958fb1834a865b57ed558c936ffd52e54d3500
IagoPFaria <iago.faria.1625316@sga.pucminas.br>
2026-06-14 19:53:51 -0300

    Update __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Pacote de apoio do jogo Seven Days of Fear.
+
+Esta pasta organiza configurações, dados, funções auxiliares,
+sprites e uma entrada alternativa para executar o jogo.
+"""
+
+NOME_JOGO = "Seven Days of Fear"
